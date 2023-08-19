f9015277afbba3966f90b9eed7fe3694446360b7
Nastya <ctanv22@gmail.com>
2023-08-19 15:11:36 +0300

    Lesson14

---
+++ Lesson14/src/main/java/MainApp.java
@@ -0,0 +1,22 @@
+public class MainApp {
+    public static void main(String[] args) throws InterruptedException {
+
+    Counter counter = new Counter();
+
+    Thread thread1 = new Thread(new CounterThread(counter, "first:"));
+    Thread thread2 = new Thread(new CounterThread(counter, "second:"));
+    Thread thread3 = new Thread(new CounterThread(counter, "third:"));
+
+        thread3.start();
+        thread3.join();
+
+        thread2.start();
+        thread2.join();
+
+        thread1.start();
+        thread1.join();
+
+        System.out.println(counter.getCount());
+
+    }
+}
