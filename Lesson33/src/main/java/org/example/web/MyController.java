de0d36bddeec9d531e8ed925d69c18f16c0b1e4e
Nastya <ctanv22@gmail.com>
2023-10-22 11:03:43 +0300

    Lesson33

---
+++ Lesson33/src/main/java/org/example/web/MyController.java
@@ -0,0 +1,37 @@
+package org.example.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.Contract;
+import org.example.service.ContractService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@Controller
+@ControllerAdvice
+@RequestMapping("/mypage")
+public class MyController {
+
+private final ContractService contractService;
+
+    @GetMapping
+    public String page(){
+        return "mypage";
+    }
+
+    @PostMapping
+    public ModelAndView addContract(@ModelAttribute (name="contract") @Valid Contract contract, BindingResult result){
+
+        ModelAndView modelAndView = new ModelAndView("mypage");
+
+        if(!result.hasFieldErrors()){
+            contractService.save(contract);
+        }
+        modelAndView.addObject("contracts", contractService.getContract());
+        return modelAndView;
+    }
+}
