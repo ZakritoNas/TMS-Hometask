2531dfa16a39d9a556dcfc3d1d1f2866cd6ba2f7
Nastya <ctanv22@gmail.com>
2023-11-04 11:20:50 +0300

    Lesson36

---
+++ Lesson36/src/main/java/dto/PersonSearchDto.java
@@ -0,0 +1,26 @@
+package dto;
+
+import entity.TaskEntity;
+import enums.PersonType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PersonSearchDto {
+
+    private String name;
+    private PersonType type;
+    private LocalDate birthdayFrom;
+    private LocalDate birthdayTo;
+    private Boolean isMan;
+    private List<TaskEntity> tasks;
+
+}
