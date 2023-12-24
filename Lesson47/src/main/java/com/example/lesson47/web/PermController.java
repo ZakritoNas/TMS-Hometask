9bc0ac54ba5f29ee292c7e3813afcfbd29fda7b1
Nastya <ctanv22@gmail.com>
2023-12-24 14:16:37 +0300

    Lesson47

---
+++ Lesson47/src/main/java/com/example/lesson47/web/PermController.java
@@ -0,0 +1,14 @@
+package com.example.lesson47.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PermController {
+    @GetMapping("special")
+        public String special(){
+            return "special";
+        }
+}
